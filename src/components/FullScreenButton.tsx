610e6e1597de2372525154b9e514765b231bcee1
Alisa Seletskaya <sealisaa@yandex.ru>
2023-10-13 12:04:39 +0300

    refactor PlayerPage

---
+++ src/components/FullScreenButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FullScreenButton: React.FC = () => {
+  return (
+    <button type="button" className="player__full-screen">
+      <svg viewBox="0 0 27 27" width="27" height="27">
+        <use xlinkHref="#full-screen"></use>
+      </svg>
+      <span>Full screen</span>
+    </button>
+  );
+}
+
+export default FullScreenButton;
--- src/pages/player-page/PlayerPage.tsx
+++ src/pages/player-page/PlayerPage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FullScreenButton from "../../components/FullScreenButton";
 
 const PlayerPage: React.FC = () => {
   return (
@@ -16,6 +17,8 @@ const PlayerPage: React.FC = () => {
           <div className="player__time-value">1:30:29</div>
         </div>
 
+        // TODO: придумать что-то с PlayButton
+
         <div className="player__controls-row">
           <button type="button" className="player__play">
             <svg viewBox="0 0 19 19" width="19" height="19">
@@ -24,13 +27,7 @@ const PlayerPage: React.FC = () => {
             <span>Play</span>
           </button>
           <div className="player__name">Transpotting</div>
-
-          <button type="button" className="player__full-screen">
-            <svg viewBox="0 0 27 27" width="27" height="27">
-              <use xlinkHref="#full-screen"></use>
-            </svg>
-            <span>Full screen</span>
-          </button>
+          <FullScreenButton />
         </div>
       </div>
     </div>
